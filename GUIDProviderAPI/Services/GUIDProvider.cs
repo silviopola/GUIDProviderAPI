04bd6055f625bf84f097d6b37ecac8adafa1a4af
Silvio Pola <silvio.pola@gmail.com>
2024-01-06 16:15:37 +0100

    Update GUIDProvider.cs description about lazy init

--- GUIDProviderAPI/Services/GUIDProvider.cs
+++ GUIDProviderAPI/Services/GUIDProvider.cs
@@ -4,7 +4,8 @@ namespace GUIDProviderAPI.Services
     public class GUIDProvider : IGUIDProvider
     {
         private Guid _guid = Guid.Empty;
-            
+
+        // Performing a Lazy initialization so a unique guid for any class instance
         public Guid ProvideGUID()
         {
            if (_guid == Guid.Empty)
